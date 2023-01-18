e8d6613199cf16687023a004d4390258338266d3
Craig Donald <1craig.donald@gmail.com>
2023-01-18 11:29:26 +0000

    Example tests for Tanya's page

--- src/components/TanyasPage.cy.js
+++ src/components/TanyasPage.cy.js
@@ -4,5 +4,8 @@ describe('<TanyasPage />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-vue
     cy.mount(TanyasPage)
+    cy.get('#counterblock').should('have.text', 'Counter is 0')
+    cy.get('#counterbutton').should('have.text', 'Increment counter').click()
+    cy.get('#counterblock').should('have.text', 'Counter is 5')
   })
 })
