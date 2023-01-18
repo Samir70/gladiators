aae9d87ce7ba79289726667aabae52b35a9b4a8f
Tanya Sahin <tanyasahin123@gmail.com>
2023-01-18 16:07:19 +0000

    Profile page and Cypress testing

--- src/components/ProfilePage.vue
+++ src/components/ProfilePage.vue
@@ -4,7 +4,35 @@
 </script>
 
 <template>
-  <h1>Profile Page</h1>
+  
+  <div id="homebutton">
+    <button>
+      <h2>Home</h2>
+    </button>
+  </div>
+
+  <div id="profmenu">
+      <h1 id="profile-title">My Profile</h1>
+  
+    <br>
+    <button>
+      <h2 id="goals-button">Goals</h2>
+    </button>
+    <br>
+    <button>
+      <h2 id="history-button">History</h2>
+    </button>
+    <br>
+    <button>
+      <h2 id="timesheet-button">Timesheet</h2>
+    </button>
+    <br>
+    <button>
+      <h2 id="calculator-button">Calculator</h2>
+    </button>
+  </div>
+
+
 
 </template>
 
