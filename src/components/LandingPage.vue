4514744e2df05cc70e35db5ece9c8981a0e33eb9
Samir70 <sbetmouni@talktalk.net>
2023-01-18 15:06:14 +0000

    landing page passes test

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -4,18 +4,12 @@
 </script>
 
 <template>
-  <h1 style = "color: #4F182B;">Landing Page</h1>
-
-<p>colour pallete 1</p>
-<p1>tyrian purple</p1>
-<p2></p2>
+  <h1 id="landingpage-title">Landing Page</h1>
 </template>
 
 <style scoped>
 /* colouor pallete idea */
-p1 {
-    color: #4F182B;}
-
-
-
+h1 {
+  color: #4F182B;
+}
 </style>
