3242d65dbe7b179e6ca015562f30109080f56299
Craig Donald <1craig.donald@gmail.com>
2023-01-19 16:58:08 +0000

    Exercise Catalogue layout

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="/hello">Hello World</a> -->
     <!-- <router-link to="/hello">Hello world</router-link> 
     <router-link to="/tanyaspage">Tanya's Page</router-link> -->
-    <router-link to="/exercisecatalogue">Exercise Catalogue</router-link>
+    <!-- <router-link to="/exercisecatalogue">Exercise Catalogue</router-link> -->
   </div>
   <!-- <HelloWorld msg="Will this work?" /> -->
   <router-view></router-view>
--- src/components/ExerciseCatalogue.vue
+++ src/components/ExerciseCatalogue.vue
@@ -1,29 +1,102 @@
 <script setup>
-import GlassBubble from './GlassBubble.vue';
+import GlassBubble from "./GlassBubble.vue";
 import ExerciseBody from "./ExerciseCatalogueBody.vue";
 </script>
 
 <template>
-  <div id="header">
-    <h1>All Exercises Catalogue</h1>
+  <div id="ExerciseCatalogue">
+    <div id="header">
+      <h1>All Exercises Catalogue</h1>
+      <!--
+      - back to dashboard button
+      - logout button?
+      - profile buton?
+  -->
+    </div>
+
+    <div id="Body">
+        <ExerciseBody >
+        </ExerciseBody>
+    </div>
+
+    <div id="dropdown-for-type">
+      <GlassBubble id="exercises-bubble">
+        <p>Training Type</p>
+        <label>Exercises</label>
+        <select>
+          <option id="strength" value="strength">Strength</option>
+          <!-- figure out if statement, maybe use buttons instead -->
+
+
+          <option id="flexibility" value="flexibility">Flexibility</option>
+          <option id="cardio" value="cardio">Cardio</option>
+        </select>
+      </GlassBubble>
+
+    </div>
+
+    <!-- If statement to show specific dropdown -->
+    
+    <div id="strength">
+      <GlassBubble id="selection-bubble">
+        <p>Pick an exercise to see the parts used</p>
+        <label>Exercises</label>
+        <select>
+          <option value="Curls">Curls</option>
+          <option value="Squats">Squats</option>
+          <option value="Pushups">Pushups</option>
+        </select>
+      </GlassBubble>
+    </div>
+
+    <div id="placeholder">
+      <p>Pick summat else</p>
+    </div>
+
+
+    
+
+    <!-- Category filter buttons, cardio, strength, stretches -->
   </div>
 
-  <div id="topnav-buttons">
-    <!--
-    - back to dashboard button
-    - logout button?
-    - profile buton?
--->
-  </div>
-
-
-  <GlassBubble id="muscles">
-    <ExerciseBody>
-    </ExerciseBody>
-  </GlassBubble>
-
-  <!-- Category filter buttons, cardio, strength, stretches -->
 </template>
 
 <style scoped>
+
+#Body {
+  position: absolute;
+  left: 20px;
+  top: -100px;
+}
+
+
+
+#exercises-bubble{
+  padding: 10px;
+  width: 200px
+}
+
+#selection-bubble{
+  padding: 10px;
+  width: 200px
+}
+
+#dropdown-for-type{
+  position: absolute;
+  right: 9pc;
+  top: 10pc
+}
+
+#dropdown-for-exercise {
+  position: absolute;
+  right: 9pc;
+  top: 17pc;
+}
+
+#placeholder {
+  position: absolute;
+  right: 9pc;
+  top: 17pc;
+}
+
 </style>
