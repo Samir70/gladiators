ffa52113f465c18fb134a59b95470bd03c3ecdcc
Samir70 <sbetmouni@talktalk.net>
2023-01-19 10:19:04 +0000

    restyle landing page

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -3,38 +3,48 @@ import GlassBubble from './GlassBubble.vue';
 
 </script>
 
-<template> 
-  <GlassBubble id="welcome-bubble">
-    <h1 id="landingpage-welcome">Welcome To Gladimakers</h1>
-    <img id="gladiator-thumbsup" src="../../public/GladiatorThumbsUp.png" />
-    <p id="welcome-blurb">Even if your name isn't Maximus Decimus Meridius, we can still help you achieve your goals.</p>
-  </GlassBubble>
+<template>
+  <div id="landingpage">
+    <GlassBubble id="welcome-bubble">
+      <h1 id="landingpage-welcome">Welcome To Gladimakers</h1>
+      <img id="gladiator-thumbsup" src="../../public/GladiatorThumbsUp.png" />
+      <p id="welcome-blurb">Even if your name isn't Maximus Decimus Meridius, we can still help you achieve your goals.
+      </p>
+    </GlassBubble>
 
-  <GlassBubble id="buttons-bubble">
-    <button>Sign In</button>
-    <button>Sign Up</button>
-  </GlassBubble>
+    <GlassBubble id="buttons-bubble">
+      <button>Sign In</button>
+      <button>Sign Up</button>
+    </GlassBubble>
+  </div>
 </template>
 
 <style scoped>
 /* colour pallete idea */
+#landingpage {
+  width: 85vw;
+}
 #landingpage-welcome {
   margin: auto;
   width: fit-content;
   font-size: large;
 }
+
 #gladiator-thumbsup {
   width: 65vw;
 }
+
 #welcome-blurb {
   width: 60vw;
   margin: auto;
 }
+
 #welcome-bubble {
   float: right;
   top: 50px;
   width: 65vw;
 }
+
 #buttons-bubble {
   display: flex;
   flex-direction: column;
@@ -42,8 +52,10 @@ import GlassBubble from './GlassBubble.vue';
   position: absolute;
   float: left;
   top: 50;
-  width: 25vw;
+  padding: 0px 20px;
+  max-width: 25vw;
 }
+
 button {
   width: fit-content;
   margin: 2px auto;
