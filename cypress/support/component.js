40e289bec166ff8fd9e3547d9b86686fbf3620e8
Samir70 <sbetmouni@talktalk.net>
2023-01-20 06:26:58 +0100

    modify cy.mount to include router

--- cypress/support/component.js
+++ cypress/support/component.js
@@ -19,9 +19,38 @@ import './commands'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
+// ******** original
+// import { mount } from 'cypress/vue'
+
+// Cypress.Commands.add('mount', mount)
+
+// // Example use:
+// // cy.mount(MyComponent)
+// ******************* end of original
+
 import { mount } from 'cypress/vue'
+import { createMemoryHistory, createRouter } from 'vue-router'
+import { routes } from '../../src/routes'
 
-Cypress.Commands.add('mount', mount)
+Cypress.Commands.add('mount', (component, options = {}) => {
+  // Setup options object
+  options.global = options.global || {}
+  options.global.plugins = options.global.plugins || []
 
-// Example use:
-// cy.mount(MyComponent)
+  // create router if one is not provided
+  if (!options.router) {
+    options.router = createRouter({
+      routes: routes,
+      history: createMemoryHistory(),
+    })
+  }
+
+  // Add router plugin
+  options.global.plugins.push({
+    install(app) {
+      app.use(options.router)
+    },
+  })
+
+  return mount(component, options)
+})
