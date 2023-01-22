bd1c8811278293c0c169e63f8162876ee688516d
Samir70 <sbetmouni@talktalk.net>
2023-01-22 11:53:54 +0000

    remove css that does nothing

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,12 +11,9 @@ const user = ref(store.state.user)
 
 </script>
 <template>
+    <h1>Gladiator Dashboard</h1>
+    <p v-if="user">{{ user.username }}</p>
     <div id="dashboard">
-        <h1>Gladiator Dashboard</h1>
-        <p v-if="user">{{ user.username }}</p>
-
-        <!-- Navigation -->
-
         <GlassBubble id="dash-nav-bubble">
             <ProfileButton />
 
@@ -36,83 +33,71 @@ const user = ref(store.state.user)
             </button>
         </GlassBubble>
 
-        <!-- Utilities -->
-        <GlassBubble id="utilities-bubble">
-
-            <p>Utilities</p>
-
-            <br>
-
-            <button class="button" @click="$router.push('/')">
-                <p>Log Out</p>
-            </button>
-
-            <br>
-
-            <button class="button" @click="$router.push('guideandtipspage')">
-                <p>User Guide</p>
-            </button>
-
-            <br>
-
-            <button class="button">
-                <p>Facilities Finder</p>
-            </button>
-
-            <br>
-
-            <button class="button">
-                <p>Leaderboard</p>
-            </button>
-
-            <br>
-
-            <button class="button">
-                <p>Lifting Calculator</p>
-            </button>
-
-            <br>
-
-            <button class="button">
-                <p>Stop Watch</p>
-            </button>
-        </GlassBubble>
-
-
-        <!-- Exercise catalogue -->
-        <GlassBubble id="exercise-catalogue-bubble">
-
-            <button class="button" @click="$router.push('exercisecatalogue')">
-                <p>Exercise Catalogue</p>
-            </button>
-        </GlassBubble>
+        <div id="catalogue-current-container">
+            <GlassBubble id="exercise-catalogue-bubble">
+                <button class="button" @click="$router.push('exercisecatalogue')">
+                    <p>Exercise Catalogue</p>
+                </button>
+            </GlassBubble>
+            <GlassBubble id="current-workout-bubble">
+                <p>current workout</p>
+                <button>
+                    <p>Random Workout Generator</p>
+                </button>
+                <button>
+                    <p>Workout Complete</p>
+                </button>
+                <button>
+                    <p>Clear All</p>
+                </button>
+    
+                <p>(remove individual exercise button, for each, similar to like/unlike idea)</p>
+    
+            </GlassBubble>
+        </div>
 
         <br>
 
-        <GlassBubble id="current-workout-bubble">
-            <p>current workout</p>
 
-            <br>
+        <GlassBubble id="utilities-bubble">
+                <p>Utilities</p>
 
-            <button class="button">
-                <p>Random Workout Generator</p>
-            </button>
+                <br>
 
-            <br>
+                <button class="button" @click="$router.push('/')">
+                    <p>Log Out</p>
+                </button>
 
-            <button class="button">
-                <p>Workout Complete</p>
-            </button>
+                <br>
 
-            <br>
+                <button class="button" @click="$router.push('guideandtipspage')">
+                    <p>User Guide</p>
+                </button>
 
-            <button class="button">
-                <p>Clear All</p>
-            </button>
+                <br>
 
-            <p>(remove individual exercise button, for each, similar to like/unlike idea)</p>
+                <button class="button">
+                    <p>Facilities Finder</p>
+                </button>
 
-        </GlassBubble>
+                <br>
+
+                <button class="button">
+                    <p>Leaderboard</p>
+                </button>
+
+                <br>
+
+                <button class="button">
+                    <p>Lifting Calculator</p>
+                </button>
+
+                <br>
+
+                <button class="button">
+                    <p>Stop Watch</p>
+                </button>
+            </GlassBubble>
 
     </div>
 </template>
@@ -122,14 +107,15 @@ const user = ref(store.state.user)
 
 <style scoped>
 #dashboard {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
     width: 95vw;
 }
 
-body,
-.glass_bubble::before {
-    background-position: 40%;
-    background-size: cover;
-    background-attachment: fixed;
+#catalogue-current-container {
+    display: flex;
+    flex-direction: column;
 }
 
 button img {
@@ -138,29 +124,12 @@ button img {
     border-radius: 80px;
 }
 
-/* .background {
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-} */
-
-h1,
-h2 {
+h1 {
     font-family: 'Roboto', sans-serif;
     color: rgb(0, 0, 0);
-}
-
-h1 {
     font-size: 4vmin;
     letter-spacing: .6vmin;
     text-transform: uppercase;
-}
-
-h2 {
-    font-size: 2.7vmin;
-    letter-spacing: .3vmin;
 }
 
 #dash-nav-bubble {
@@ -173,74 +142,21 @@ h2 {
 #utilities-bubble {
     width: 30vmin;
     height: 90vmin;
-    float: right;
-    /* position: adsolute; */
-    /* top: 300px; */
 }
 
 #exercise-catalogue-bubble {
     width: 60vmin;
     height: 30vmin;
-    position: absolute;
-    left: 350px;
-    /* margin-left: auto;
-  margin-right: auto; */
 }
 
 #current-workout-bubble {
-    position: relative;
+    display: flex;
+    flex-direction: column;
     width: 60vmin;
     height: 55vmin;
-    position: absolute;
-    left: 350px;
-    top: 450px;
-
-    /* margin-left: auto;
-  margin-right: auto; */
-}
-
-/* .innercard {
-    position: relative;
-    width: 70vmin;
-    height: 50vmin;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-} */
-
-.text {
-    position: absolute;
-    top: 16vmin;
 }
 
 @media (min-width: 731px) {
 
-    h1 {
-        font-size: 3vmin;
-    }
-
-    h2 {
-        font-size: 2.4vmin;
-    }
-
-    .card,
-    .innercard {
-        width: 60vmin;
-        height: 45vmin;
-    }
-
-    .text {
-        top: 15vmin;
-    }
-
-    /* 
-    .button {
-        bottom: 16vmin;
-    }
-
-    button {
-        width: 25vmin;
-        height: 7vmin;
-    } */
 }
 </style>
