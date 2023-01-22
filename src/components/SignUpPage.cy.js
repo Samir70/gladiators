6e50d000587946a2037b12f5f3fce4f05d63ae2e
Samir70 <sbetmouni@talktalk.net>
2023-01-22 08:23:16 +0000

    add signup tests for username/email in use

--- src/components/SignUpPage.cy.js
+++ src/components/SignUpPage.cy.js
@@ -37,4 +37,72 @@ describe("<SignUpPage />", () => {
     cy.get("#home-button").click();
     // cy.url().should("include", "dashboard")
   });
+  it("Shows error for username in use", () => {
+    const newUser = {
+      username: "Maximus Decimus Meridius",
+      email: "winner@colosseum.com",
+      password: "MarcusAurelius",
+    };
+    const serverResponse = {
+      signUpFailed: true,
+      msg: "that username is in use",
+    };
+    cy.mount(SignUpPage);
+    cy.intercept("/.netlify/functions/addUser*", async (req) => {
+      req.reply(serverResponse);
+    }).as("fetch");
+    cy.get("#signup-bubble").get("h1").should("contain", "Sign Up");
+    cy.get("#signup-bubble")
+      .get("#name-field")
+      .should("have.attr", "type", "text")
+      .type(newUser.username);
+    cy.get("#signup-bubble")
+      .get("#email-field")
+      .should("have.attr", "type", "email")
+      .type(newUser.email);
+    cy.get("#signup-bubble")
+      .get("#password-field")
+      .should("have.attr", "type", "password")
+      .type(newUser.password);
+    cy.get("#signup-bubble").get("#signup-button").click();
+    cy.get("#home-button").click();
+    cy.get("#signup-failed-error").should(
+      "contain",
+      "Unable to sign up because: that username is in use"
+    );
+  });
+  it("Shows error for email in use", () => {
+    const newUser = {
+      username: "Maximus Decimus Meridius",
+      email: "winner@colosseum.com",
+      password: "MarcusAurelius",
+    };
+    const serverResponse = {
+      signUpFailed: true,
+      msg: "that email is in use",
+    };
+    cy.mount(SignUpPage);
+    cy.intercept("/.netlify/functions/addUser*", async (req) => {
+      req.reply(serverResponse);
+    }).as("fetch");
+    cy.get("#signup-bubble").get("h1").should("contain", "Sign Up");
+    cy.get("#signup-bubble")
+      .get("#name-field")
+      .should("have.attr", "type", "text")
+      .type(newUser.username);
+    cy.get("#signup-bubble")
+      .get("#email-field")
+      .should("have.attr", "type", "email")
+      .type(newUser.email);
+    cy.get("#signup-bubble")
+      .get("#password-field")
+      .should("have.attr", "type", "password")
+      .type(newUser.password);
+    cy.get("#signup-bubble").get("#signup-button").click();
+    cy.get("#home-button").click();
+    cy.get("#signup-failed-error").should(
+      "contain",
+      "Unable to sign up because: that email is in use"
+    );
+  });
 });
