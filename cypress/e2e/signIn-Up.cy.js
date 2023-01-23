537c94480a11ce7136b9aa9c2782798b397655dd
Samir70 <sbetmouni@talktalk.net>
2023-01-23 10:50:59 +0000

    tests signin

---
+++ cypress/e2e/signIn-Up.cy.js
@@ -0,0 +1,26 @@
+describe("template spec", () => {
+  it("Can get to the signIn page and sign in", () => {
+    const user = {
+      username: "Maximus Decimus Meridius",
+      email: "winner@colosseum.com",
+      password: "MarcusAurelius",
+    };
+    cy.visit("http://localhost:8888/#/");
+    cy.get("#buttons-bubble")
+      .find("button")
+      .eq(0)
+      .should("contain", "Sign In")
+      .click();
+    cy.get("#signin-bubble")
+      .get("#email-field")
+      .should("have.attr", "type", "email")
+      .type(user.email);
+    cy.get("#signin-bubble")
+      .get("#password-field")
+      .should("have.attr", "type", "password")
+      .type(user.password);
+    cy.get("#signin-bubble").get("#signin-button").click();
+    cy.url().should("include", "dashboard");
+    // cy.get("#buttons-bubble").find("button").eq(1).should("contain", "Sign Up").click()
+  });
+});
