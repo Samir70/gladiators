a446ae99f20cacd4830a031badf65094974026f9
Samir70 <sbetmouni@talktalk.net>
2023-01-23 15:26:07 +0000

    add dummy workout

--- src/store.js
+++ src/store.js
@@ -1,19 +1,35 @@
 import { createStore } from "vuex";
 export const store = createStore({
-    state() {
-        return {
-            count: 0, 
-            user: {username:"Steve"}
-        }
-    }, 
-    mutations: {
-        increment(state, payload) {
-            console.log("store.increment payload is", payload)
-            state.count += payload
+  state() {
+    return {
+      count: 0,
+      user: { username: "Steve" },
+      currentworkout: [
+        {
+          _id: curlID,
+          name: "curl",
+          tags: ["arms", "equipped", "hypertrophy", "strength"],
+          experience: "B",
+          bodyarea: "upper",
         },
-        login(state, payload) {
-            console.log("store.login payload is", payload)
-            state.user = payload
-        }
-    }
-})
+        {
+          _id: squatID,
+          name: "squat",
+          tags: ["legs", "equipped"],
+          experience: "A",
+          bodyarea: "lower",
+        },
+      ],
+    };
+  },
+  mutations: {
+    increment(state, payload) {
+      console.log("store.increment payload is", payload);
+      state.count += payload;
+    },
+    login(state, payload) {
+      console.log("store.login payload is", payload);
+      state.user = payload;
+    },
+  },
+});
