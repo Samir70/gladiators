636a627ee65c914780871ee923f3d0f085737f2a
Samir70 <sbetmouni@talktalk.net>
2023-01-24 14:26:18 +0000

    test that unitComplete is emitted

--- src/components/ExerciseTimer.cy.js
+++ src/components/ExerciseTimer.cy.js
@@ -2,9 +2,10 @@ import ExerciseTimer from "./ExerciseTimer.vue";
 
 describe("<ExerciseTimer />", () => {
   it("renders with needed elements", () => {
+    const onUnitCompleteSpy = cy.spy().as("onUnitCompleteSpy")
     cy.clock();
     cy.mount(ExerciseTimer, {
-      props: { exerciseTime: 5, restTime: 10, exerciseName: "Star Jumps" },
+      props: { exerciseTime: 5, restTime: 10, exerciseName: "Star Jumps", onUnitComplete: onUnitCompleteSpy },
     });
     cy.get("#exercise-timer-bubble").get("#start-workout-button").click();
     cy.get("#exercise-timer-bubble")
@@ -14,5 +15,6 @@ describe("<ExerciseTimer />", () => {
     cy.get("#exercise-timer-bubble")
       .get("#workout-action")
       .should("contain", "Do Star Jumps for 0 seconds");
+      cy.get("@onUnitCompleteSpy").should("have.been.called")
   });
 });
