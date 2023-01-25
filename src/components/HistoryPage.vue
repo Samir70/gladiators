6cff2647dc167883a4a2fa14f373361525e64e5e
Andrew <andynguyen93@hotmail.co.uk>
2023-01-25 10:16:23 +0000

    history page vue-cal

--- src/components/HistoryPage.vue
+++ src/components/HistoryPage.vue
@@ -1,6 +1,42 @@
-<script setup>
-import GlassBubble from './GlassBubble.vue';
+<script>
+  import GlassBubble from './GlassBubble.vue';
 
+
+// import datepicker from "vue-awesome-datepicker";
+// import "vue-awesome-datepicker/dist/font-default.css"; //if you want default fonts
+// export default {
+//   components: {
+//     datepicker,
+//   },
+//   data() {
+//     return { date: {},dateGreg:{} };
+//   },
+// };
+import vueCal from 'vue-cal'
+import 'vue-cal/dist/vuecal.css'
+
+export default {
+  data: () => ({
+    events: [
+      {
+        title: 'Event 1',
+        start: new Date(2023, 0, 25),
+        end: new Date(2023, 0, 25),
+        allDay: true,
+        class: 'red-event'
+      },
+      {
+        title: 'Event 2',
+        start: new Date(2023, 0, 26),
+        end: new Date(2023, 0, 26),
+        class: 'blue-event'
+      }
+    ]
+  }),
+  components: {
+    vueCal
+  }
+}
 </script>
 
 <template>
@@ -18,12 +54,26 @@ import GlassBubble from './GlassBubble.vue';
 
 
     <div id="bodycontainer">
+  
+  <div>
+<vue-cal
+    class="vuecal--date-picker"
+    xsmall
+    hide-view-selector
+    :time="false"
+    active-view="month"
+    :disable-views="['week']"
+    :events="events"
+    style="width: 540px;height: 600px">
+</vue-cal>
+
+
+  </div>
 
 
 
 
 
-      
     </div>
 
 
@@ -33,4 +83,7 @@ import GlassBubble from './GlassBubble.vue';
 #profilecontainer {
   width: 85vw;
 }
+
+
+
 </style>
