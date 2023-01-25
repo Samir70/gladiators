686a41d12cd0edf6939079ef26ccc1ca29c4732e
Ibrahim Pala <ibrahimpala@MASTAFF27QRLH6V.broadband>
2023-01-25 14:43:22 +0000

    Change yml structure

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  component: {
-    devServer: {
-      framework: "vue",
-      bundler: "vite",
 
-    },
-  },
   e2e: {
     baseUrl: 'http://localhost:8888',
     supportFile: false
   },
+  component: {
+    devServer: {
+      framework: "vue",
+      bundler: "vite",
+    }
+  }
 });
