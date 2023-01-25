e6ffc323446ec3efd3f217b69775b5ea6bd7bac3
Ibrahim Pala <ibrahimpala@MASTAFF27QRLH6V.broadband>
2023-01-25 14:52:51 +0000

    test

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,16 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
 
-  e2e: {
-    baseUrl: 'http://localhost:8888',
-    supportFile: false
-  },
+  
   component: {
     devServer: {
       framework: "vue",
       bundler: "vite",
-    }
-  }
+
+    },
+  },
+  e2e: {
+    baseUrl: 'http://localhost:8888',
+    supportFile: false
+  },
 });
