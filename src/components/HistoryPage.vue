4496602c39083c8e06ed2f23de7103a01f444fdb
Andrew <andynguyen93@hotmail.co.uk>
2023-01-26 11:17:34 +0000

    bug fixes and UI improvements

--- src/components/HistoryPage.vue
+++ src/components/HistoryPage.vue
@@ -10,9 +10,12 @@ import moment from 'moment'
 
 const user = ref(store.state.user)
 
+let loggedIn = ref(user.value === null ? false : true)
+
 let userHistory = ref("")
 let userWorkoutCount = ref("")
 let events = ref([])
+let showCalendar = ref(false)
 
 const getHistory = async () => {
   let result = await fetch(`/.netlify/functions/getHistory`, {
@@ -23,32 +26,33 @@ const getHistory = async () => {
       return response
         .json()
     }).then(function (data) {
-      userHistory = data; userWorkoutCount = userHistory.workout.length;
-      for (let i = 0; i < userWorkoutCount; i++) {
+      userHistory = data;
+      userWorkoutCount.value = userHistory.workout.length;
+      showCalendar.value = true;
+      // console.log(userWorkoutCount)
+      for (let i = 0; i < userWorkoutCount.value; i++) {
         let list = '<ul>'
         userHistory.workout[i].exercises.forEach(item => {
           list += `<li>${item}</li>`
         })
         events.value.push({
-          title: "Workout " + (Number(i)+1),
+          title: "Workout " + (Number(i) + 1),
           start: moment(userHistory.workout[i].date).format('YYYY-MM-DD'),
           end: moment(userHistory.workout[i].date).format('YYYY-MM-DD'),
           content: list,
           class: 'done'
         });
-        console.log(userWorkoutCount)
-        const calendarReveal = document.getElementById("workoutcalendar")
-        calendarReveal.style = "display:block"
-        const workoutCounter = document.getElementById("workoutCounter")
-        workoutCounter.style = "display:block"
-        document.getElementById("revealbutton").disabled = true;
+        // const calendarReveal = document.getElementById("workoutcalendar")
+        // calendarReveal.style = "display:block"
+        // document.getElementById("revealbutton").disabled = true;
 
       }
 
     })
 }
-
-
+if (loggedIn.value) {
+  getHistory()
+}
 </script>
 
 <template>
@@ -65,12 +69,12 @@ const getHistory = async () => {
     </div>
 
   </GlassBubble>
-  <h2 id="workoutCounter" style="display:none"> You have worked out {{ userWorkoutCount}} times since you began your journey! </h2>
-  
-  <button id="revealbutton" v-on:click="getHistory">View your history</button>
 
+  <h2 id="workoutCounter" v-if="loggedIn"> You have worked out {{ userWorkoutCount }} times since you began your
+    journey! </h2>
+  <h2 v-else> You are not logged in.</h2>
 
-  <div id=workoutcalendar style="display:none">
+  <div id=workoutcalendar v-if="showCalendar">
     <vue-cal class="vuecal--date-picker" xsmall hide-view-selector :time="false" active-view="month"
       :disable-views="['week']" :events=events style="width: 650px;height: 600px;">
     </vue-cal>
