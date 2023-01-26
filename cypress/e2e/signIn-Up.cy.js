fd3aabba7f61b5f8542917e7f5ad16659e02d384
Samir70 <sbetmouni@talktalk.net>
2023-01-26 09:26:32 +0000

    add test to signup new user

--- cypress/e2e/signIn-Up.cy.js
+++ cypress/e2e/signIn-Up.cy.js
@@ -1,12 +1,14 @@
 describe("template spec", () => {
+  before(() => {
+    cy.task("connect");
+    cy.task("resetUsers", "form SignUp e2e");
+  });
   it("Can get to the signIn page and sign in", () => {
     const user = {
       username: "Wolf",
       email: "wolf@email.com",
       password: "wolf",
     };
-    cy.task("connect")
-    cy.task("resetUsers", "form SignUp e2e")
     cy.visit("http://localhost:8888/#/");
     cy.get("#buttons-bubble")
       .find("button")
@@ -24,5 +26,32 @@ describe("template spec", () => {
     cy.get("#signin-bubble").get("#signin-button").click();
     cy.url().should("include", "dashboard");
     // cy.get("#buttons-bubble").find("button").eq(1).should("contain", "Sign Up").click()
+  });
+  it("Can sign up with a new email", () => {
+    const user = {
+      username: "Maximus Decimus Meridius",
+      email: "winner@colosseum.com",
+      password: "MarcusAurelius",
+    };
+    cy.visit("http://localhost:8888/#/");
+    cy.get("#buttons-bubble")
+      .find("button")
+      .eq(1)
+      .should("contain", "Sign Up")
+      .click();
+    cy.get("#signup-bubble")
+      .get("#name-field")
+      .should("have.attr", "type", "text")
+      .type(user.username);
+    cy.get("#signup-bubble")
+      .get("#email-field")
+      .should("have.attr", "type", "email")
+      .type(user.email);
+    cy.get("#signup-bubble")
+      .get("#password-field")
+      .should("have.attr", "type", "password")
+      .type(user.password);
+    cy.get("#signup-bubble").get("#signup-button").click();
+    cy.url().should("include", "survey");
   });
 });
